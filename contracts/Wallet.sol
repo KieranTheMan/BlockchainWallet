2bee1d6b0ba1c3b2e810e118ee21ab5335c4f32c
KieranTheMan <kieran9fowler@gmail.com>
2020-12-22 13:38:54 +0000

    create wallet.sol file and copy completed Solidity code from Remix

---
+++ contracts/Wallet.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+pragma experimental ABIEncoderV2;
+
+contract Wallet {
+    address[] public approvers;
+    uint public quorum;//public creats a function
+    struct Transfer {
+        uint id;
+        uint amount;
+        address payable to;
+        uint approvals;
+        bool sent;
+    }
+    
+    Transfer[] public transfers;
+    //define a maaping to record who has recorded what
+    
+    mapping(address => mapping(uint => bool)) public approvals;
+    
+    constructor(address[] memory _approvers, uint _quorum) {
+        approvers = _approvers;
+        quorum = _quorum;
+    }
+    
+    //getter function in solidity will only return a single element, so create own function
+    function getApprovers() external view returns(address[] memory) {
+        return approvers;
+    }
+    
+    //if you want the ablity to return an array of struct, use pragma experimental.
+    function getTransfers() external view returns(Transfer[] memory) {
+        return transfers;
+    }
+
+    function createTransfer(address payable to, uint amount) external onlyApprover(){
+        transfers.push(Transfer(
+            transfers.length,
+            amount,
+            to,
+            0,
+            false
+        ));
+    }
+    
+    function approveTransfer(uint id) external onlyApprover(){
+        require(transfers[id].sent == false, 'transfer has already been sent');
+        require(approvals[msg.sender][id] == false, 'can not approve transfer twice');
+        
+        approvals[msg.sender][id] = true;
+        transfers[id].approvals++;
+        
+        if(transfers[id].approvals >= quorum) {
+            transfers[id].sent = true;
+            address payable to = transfers[id].to;
+            uint amount = transfers[id].amount;
+            to.transfer(amount);
+        }
+        
+    }
+    //this is so we can send etherium to the smart contracts address without targetting a function
+    //the receive function is automaticaly triggerd.
+    receive() external payable {}
+    
+    //  create a cutome modifier to control access to functions
+    
+    modifier onlyApprover() {
+        bool allowed = false;
+        for(uint i = 0; i < approvers.length; i++) {
+            if(approvers[i] == msg.sender) {
+                allowed = true;
+            }
+        }
+        require(allowed = true, 'Only Approvers allowed');
+        _;
+    }
+    
+}
