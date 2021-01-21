5a8b93b53b00910b5dc8dd663e96e91c1fb73228
KieranTheMan <kieran9fowler@gmail.com>
2021-01-21 12:06:16 +0000

    add hooks import getWeb3, getWallet. add const web3,accounts,wallet

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from'react';
+import {getWeb3, getWallet} from './utils.js';
 
 function App() {
-  return (
+  const [web3, setWeb3] = useState(undefiend);
+  const [accounts, setAccounts] = useState(undefined);
+  const [wallet, setWallet] = useSate(undefined)
+
+  useEffect => (() => {
+    const init = async () => {
+      const web3 = getWeb3();
+      const accounts = await web3.eth.getAccounts(); //get list of accounts from Ganash
+      const wallet = await getWallet(web3);
+      setWeb3(web3);
+      setAccounts(accounts);
+      setWallet(wallet);
+    ;
+    init();
+    };
+  }, []);
+
+  if( typeof web3 === 'undefined'
+    || typeof accounts === 'undefined'
+    || typeof wallet === 'undefined') {
+    return <div>Loading...</div>
+  }
+    return (
     <div>
       MultiSig Dapp
     </div>
