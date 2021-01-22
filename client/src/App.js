f66ed7813db2b3d8e29a9704ad594cdb37537e8d
KieranTheMan <kieran9fowler@gmail.com>
2021-01-22 10:11:58 +0000

    remove arrow from useEffect, put init() outside Effects Block, add header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,43 @@
-import React, {useEffect, useState} from'react';
-import {getWeb3, getWallet} from './utils.js';
+import React, {useEffect, useState} from 'react';
+import {getWeb3, getWallet} from './U-tils.js';
+import Header from './Header.js';
+
 
 function App() {
-  const [web3, setWeb3] = useState(undefiend);
+  const [web3, setWeb3] = useState(undefined);
   const [accounts, setAccounts] = useState(undefined);
   const [wallet, setWallet] = useState(undefined);
   const [approvers, setApprovers] = useState([]);
   const [quorum, setQuorum] = useState(undefined);
 
-  useEffect => (() => {
+  useEffect(() => {
     const init = async () => {
       const web3 = getWeb3();
       const accounts = await web3.eth.getAccounts(); //get list of accounts from Ganash
       const wallet = await getWallet(web3);
-      const approvers = await wallet.methods.getApprovers().call();
+      const approvers = await wallet.methods.getApprovers().call();//'methods' key to access function of smart contract
       const quorum = await wallet.methods.quorum().call();
       setWeb3(web3);
       setAccounts(accounts);
       setWallet(wallet);
       setApprovers(approvers);
       setQuorum(quorum);
-      
-    ;
-    init();
     };
+    init();
   }, []);
 
   if( typeof web3 === 'undefined'
     || typeof accounts === 'undefined'
+    || approvers.length === 0
+    || typeof quorum === 'undefined'
     || typeof wallet === 'undefined') {
+
     return <div>Loading...</div>
   }
     return (
     <div>
       MultiSig Dapp
+      <Header approvers={approvers} quorum={quorum}/>
     </div>
   );
 }
