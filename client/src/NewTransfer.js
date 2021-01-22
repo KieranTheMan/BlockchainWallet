a4d4ccf7bd70b847765e369a5642140a44bf2a76
KieranTheMan <kieran9fowler@gmail.com>
2021-01-22 10:43:46 +0000

    add submit button, preventDefault, updateTransfer function,createTransfer

--- client/src/NewTransfer.js
+++ client/src/NewTransfer.js
@@ -1,19 +1,34 @@
 import React, { useState } from 'react';
 
-function NewTransfer () {
+function NewTransfer ({createTransfer}) {
     const [Transfer, setTransfer] = useState(undefined);
+    const submit = (e) => {
+        e.preventDefault()
+        createTransfer(transfer)
+    }//we whant to prevent the default action of the form wich is a full page reload
+    const updateTransfer = (e, field) => {
+        const value = e.target.value;
+        setTransfer({...Transfer, [field] : value })
+    }
 
     return (
         <>
             <h2>Create Transfer</h2>
-            <form>
-                <label htmlFor='amounte'>Amount</label>
+            <form  onSubmit={(e) => submit(e)}>
+                <label htmlFor='amount'>Amount</label>
                 <input
                     id='amount'
                     type= 'text'
                     onChange={e => updateTransfer(e,'amount')}
                 />
-                
+                <label htmlFor='to'>To</label>
+                <input
+                    id='to'
+                    type= 'text'
+                    onChange={e => updateTransfer(e,'to')}
+                />
+                <button>Submit</button>
+
             </form>
         </>
     );
