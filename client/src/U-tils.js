823887d174872debdf3ce7cf78ec894979e010ef
KieranTheMan <kieran9fowler@gmail.com>
2021-01-28 10:17:40 +0000

    add fallback to localHost

--- client/src/U-tils.js
+++ client/src/U-tils.js
@@ -1,27 +1,40 @@
 //cnfiguer web3 to comunicate with smart contract from the front end
 
 import Web3 from 'web3';
-import Wallet from './contracts/Wallet.json'
+import Wallet from './contracts/Wallet.json';
 //web3 to connect smart contract to frontend
 //conection to etherum local blockchain, initialisation logic
-const getWeb3 = () => {
-    //return new Web3('http://localhost:9545') // truffle local ganash(built in blockchain of truffle) host, pass url to node that runs development blockchain
 //intergrate metaMask to sign transations on the mainNet
-    return new Promise((resolve, rejected) => {
-        window.addEventListener('load', async () => {//the load event garentees that all the javascript has been loaded and chrome extentions
-                if(window.ethereum) {
-                        const web3 = new getWeb3(window.ethereum);// pass a provider object injected by metamask
-                        try {
-                            await window.ethereum.enable()// ask the user if they want to great access to dapp.
-                            resolve(web3); //enable to acess dapp
-                        } catch(error) {
-                            rejected(error);// if no to access promise will be rejected
-                        }
-                    } else if (window.web3) { //if the user is using a old version on metaMask
-                        resolve(window.web3); // window web3 will inject web3 from metamask
-                    } else {
-                        rejected('Must install MetaMask');
-                    }
+    //return new Web3('http://localhost:9545') // truffle local ganash(built in blockchain of truffle) host, pass url to node that runs development blockchain
+
+const getWeb3 = () => {    
+    return new Promise((resolve, reject) => {
+        window.addEventListener('load', async() => {//the load event garentees that all the javascript has been loaded and chrome extentions
+            //Mordn dapp browsers
+            if(window.ethereum) {
+                const web3 = new Web3(window.ethereum);// pass a provider object injected by metamask
+                try {
+                    //Request Access if needed 
+                    await window.ethereum.enable();
+                    //enable to acess dapp, Accounts are exsposed
+                    resolve(web3);
+                } catch(error) {
+                    reject(error);// if no to access promise will be rejected
+                }
+            }
+            //Legacy dapp browsers if the user is using a old version on metaMask
+            else if (window.web3) { 
+                const web3 = window.web3;
+                resolve(web3); // window web3 will inject web3 from metamask
+            } 
+            //Fallback to localHost: use dev console port by default...
+            else {
+                const provider = new Web3.providers.HttpProvider(
+                    'http://localhost:9545'
+                );
+                const web3 = new Web3(provider);
+                resolve(web3);
+            }
         });
     });
 };
